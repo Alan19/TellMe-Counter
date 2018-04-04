db48e3047a2f1c5d19037a00186d441d913fc831
Alan19 <Alan19@users.noreply.github.com>
2018-04-03 20:49:35 -0400

    Adding sorting

--- ID Checker.rb
+++ ID Checker.rb
@@ -7,7 +7,7 @@ $stdin.each do |line|
     mods[modName] += 1
     total += 1
 end
-mods.each do |key, value|
+mods.sort_by {|key, value| value}.each do |key, value|
     puts "#{key} : #{value}, #{((value.to_f/total)*100.0).round(2)}%"
 end
 puts "#{total} IDs used"
