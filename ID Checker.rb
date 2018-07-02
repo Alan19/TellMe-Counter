b21318cad1f338af30d02829e6cd9031340df33a
Alan19 <Alan19@users.noreply.github.com>
2018-07-02 18:06:25 -0400

    Add one click run option

--- ID Checker.rb
+++ ID Checker.rb
@@ -1,14 +1,35 @@
-mods = Hash.new(0)
-total = 0
-$stdin.each do |line|
-    itemName = line.split(/,+/)[1]
-    # puts itemName
-    modName = itemName[1..itemName.index(':')-1]
-    mods[modName] += 1
+def main(input_file)
+  #Check if stdin exists
+  if $stdin.tty?
+    input_file = $stdin
+  end
+
+
+  #Create hash of mod IDs
+  mods = Hash.new(0)
+  total = 0
+
+  #Read each line to check for the name of the mod the item belongs to
+  input_file.each do |line|
+    item_name = line.split(/,+/)[1]
+    # puts item_name
+    mod_name = item_name[1..item_name.index(':') - 1]
+    mods[mod_name] += 1
     total += 1
+  end
+
+  #Sort hash by the number of item IDs a mod uses in descending order
+  mods.sort_by {|key, value| value}.reverse.each do |key, value|
+    puts "#{key} : #{value}, #{((value.to_f / total) * 100.0).round(2)}%"
+  end
+
+  #Print out usage information
+  puts "#{total}/4096 (#{(total / 4096.0 * 100).round(2)}%) IDs used"
+  puts "#{4096 - total} block IDs remaining"
 end
-mods.sort_by {|key, value| value}.reverse.each do |key, value|
-    puts "#{key} : #{value}, #{((value.to_f/total)*100.0).round(2)}%"
-end
-puts "#{total}/4096 IDs used"
-puts "#{(total/4096.0*100).round(2)}%"
+
+#Finds tellme folder and use latest block id csv
+tellme_folder = Dir["../config/tellme/blocks-id-to-registryname-csv*"]
+latest_file = tellme_folder.sort {|a, b| b <=> a}[0]
+
+main(File.new(latest_file))
